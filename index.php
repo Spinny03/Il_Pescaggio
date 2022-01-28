5e3b7e21318409586ddc330838d7c39578f1b106
Spinny03 <fili.spin2003@gmail.com>
2022-01-28 14:59:06 +0100

    create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Il Pescaggio</title>
+    </head>
+    <body>
+        <?php
+            echo "My first PHP script!";
+        ?> 
+    </body>
+</html>

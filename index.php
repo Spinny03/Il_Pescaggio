9dd25c762e7a997e5c927d4d2b7f50bc0c143ccf
altassara <barle.ale@gmail.com>
2022-01-30 22:32:03 +0100

    login page done also with css

--- index.php
+++ index.php
@@ -1,28 +1,38 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <link rel="stylesheet" href="css/styles.css">
-        <link rel="icon" type="image/x-icon" href="images/favicon.ico">
-        <title>Il Pescaggio</title>
-    </head>
-    <body>
-        <div class="login">
-            <h1>Accedi</h1>
-            <h2>Accedi con i dati che hai inserito durante la registrazione.</h2>
-            <form>
-                <label for="fname">Email</label>
-                <input type="text" id="email" name="email" placeholder="name@example.com" required><br>
-                <label for="lname">Password</label>
-                <input type="text" id="lname" name="lname" placeholder="min. 8 characters" required><br>
-                <input type="checkbox" name="remeDev" value="remeDev">
-                <label for="remeDev">Ricordami su questo dispositivo</label><br>
-                <input class="ConfButton" type="submit" value="Accedi">
-            </form>
-            <p> <a class="linkToPage" href="dd1">Password dimenticata??</a> </p>
-            <p>Non hai un account? <a class="linkToPage" href="dd2">Registrati</a> </p>
+<head>
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
+    <title>Il Pescaggio</title>
+</head>
+<body>
+    <div class="container">
+        <div class="left">
+            <img src="img/logo.png" alt="ciao" id="logo">
+            <div class="log">
+                <h1>Accedi</h1>
+                <span>Accedi con i dati che hai inserito durante la registrazione.</span>
+                <form action="">
+                    <label for="email"><b>Email</b></label>
+                    <input type="text" placeholder="name@example.com" name="uname" required>
+
+                    <label for="psw"><b>Password</b></label>
+                    <input type="password" placeholder="min. 8 characters" name="psw" required>
+                    <label><input type="checkbox" id="remember" name="remember"><span id="labelCheck">Ricordami su questo dispositivo</span></label>
+                    <button type="submit" name="login" class="logbtn">Accedi</button>
+                </form>
+                <div class="pswDiv">
+                    <a href="#" class="Link">Password dimenticata?</a>
+                </div>
+            </div>
+            <div class="bottom">
+                <span>Non hai un account? <a href="#" class="Link">Registrati</a></span>
+            </div>
         </div>
-        <div class="carousel">
-            <!-- dd3 -->
+        <div class="right">
+
         </div>
-    </body>
+    </div>
+    
+</body>
 </html>

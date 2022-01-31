09dc372af305a142ad90ad0aa20e7d78c9765407
Filippo Spinella <fili.spin2003@gmail.com>
2022-01-31 13:14:25 +0100

    SingUp and responsive
    aggiunto singUp e responsive per il login e il singUp
    
    ancora da finire la pagina di registrazione

--- signUp.php
+++ signUp.php
@@ -10,23 +10,18 @@
             <div class="left">
                 <img src="images/logo.png" alt="logo" id="logo">
                 <div class="log">
-                    <h1>Accedi</h1>
-                    <span>Accedi con i dati che hai inserito durante la registrazione.</span>
+                    <h1>Registrati</h1>
+                    <span>Inserire i tuoi dati personali per creare un nuovo account</span>
                     <form action="">
                         <label for="email"><b>Email</b></label>
                         <input type="text" placeholder="name@example.com" name="email" required>
 
                         <label for="psw"><b>Password</b></label>
                         <input type="password" placeholder="min. 8 characters" name="psw" required>
-                        <label><input type="checkbox" id="remember" name="remember"><span id="labelCheck">Ricordami su questo dispositivo</span></label>
-                        <button type="submit" name="login" class="logbtn">Accedi</button>
+                        <label for="Cpsw"><b>Conferma password</b></label>
+                        <input type="password" placeholder="inserisci la stessa password" name="Cpsw" required>
+                        <button type="submit" name="login" class="logbtn">Registrati</button>
                     </form>
-                    <div class="pswDiv">
-                        <a href="#" class="Link">Password dimenticata?</a>
-                    </div>
-                </div>
-                <div class="bottom">
-                    <span>Non hai un account? <a href="signUp.php" class="Link">Registrati</a></span>
                 </div>
             </div>
             <div class="right">

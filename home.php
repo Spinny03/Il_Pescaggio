69e3966bed0466002c5c7646674504fa1bb63e9d
Spinny03 <fili.spin2003@gmail.com>
2022-02-04 15:32:02 +0100

    Sistemato 2 cose
    - generazione automatica dish e chooseDish
    - tolto un po di css da cardStyle

--- home.php
+++ home.php
@@ -13,117 +13,55 @@
         <title>HOME</title>
     </head>
     <body>
-    <div class="container">
-        <div class="navBar">
-            
-        </div>
-        <div class="chooseDish">
-            <div class="choice">
-                ciao
-            </div>
-            <div class="choice">
-                ciao
-            </div>
-            <div class="choice">
-                ciao
-            </div>
-            <div class="choice">
-                ciao
-            </div>
-            <div class="choice">
-                ciao
-            </div>
-            <div class="choice">
-                ciao
-            </div>
-        </div>
-        <div class="title">
-            <h2>Piatti Disponibili:</h2>
-        </div>
-        <div class="dish">
-            <ul class="cards">
-                <li>
-                    <a href="" class="card">
-                    <img src="https://i.imgur.com/oYiTqum.jpg" class="card__image" alt="" />
-                    <div class="card__overlay">
-                        <div class="card__header">                   
-                        <div class="card__header-text">
-                            <h3 class="card__title">Jessica Parker</h3>            
-                        </div>
-                        </div>
-                        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-                    </div>
-                    </a>      
-                </li>
+        <div class="container">
+
+        
+            <div class="navBar">
                 
-                <li>
-                    <a href="" class="card">
-                    <img src="https://i.imgur.com/2DhmtJ4.jpg" class="card__image" alt="" />
-                    <div class="card__overlay">        
-                        <div class="card__header">                 
-                        <div class="card__header-text">
-                            <h3 class="card__title">kim Cattrall</h3>
-                        </div>
-                        </div>
-                        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-                    </div>
-                    </a>
-                </li>
-            
-                <li>
-                    <a href="" class="card">
-                    <img src="https://i.imgur.com/oYiTqum.jpg" class="card__image" alt="" />
-                    <div class="card__overlay">
-                        <div class="card__header">
-                        <div class="card__header-text">
-                            <h3 class="card__title">Jessica Parker</h3>            
-                        </div>
-                        </div>
-                        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-                    </div>
-                    </a>
-                </li>
-                <li>
-                    <a href="" class="card">
-                    <img src="https://i.imgur.com/2DhmtJ4.jpg" class="card__image" alt="" />
-                    <div class="card__overlay">
-                        <div class="card__header">                
-                        <div class="card__header-text">
-                            <h3 class="card__title">kim Cattrall</h3>
-                        </div>          
-                        </div>
-                        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-                    </div>
-                    </a>
-                </li>    
-                <li>
-                    <a href="" class="card">
-                    <img src="https://i.imgur.com/2DhmtJ4.jpg" class="card__image" alt="" />
-                    <div class="card__overlay">
-                        <div class="card__header">                
-                        <div class="card__header-text">
-                            <h3 class="card__title">kim Cattrall</h3>
-                        </div>          
-                        </div>
-                        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-                    </div>
-                    </a>
-                </li>    
-                <li>
-                    <a href="" class="card">
-                    <img src="https://i.imgur.com/2DhmtJ4.jpg" class="card__image" alt="" />
-                    <div class="card__overlay">
-                        <div class="card__header">                
-                        <div class="card__header-text">
-                            <h3 class="card__title">kim Cattrall</h3>
-                        </div>          
-                        </div>
-                        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-                    </div>
-                    </a>
-                </li>    
+            </div>
+
+
+            <div class="chooseDish">  
+                <?php
+                    for($i=0;$i<6;$i++){
+                        echo'
+                        <div class="choice"> ciao</div>';
+                        
+                    }
+                ?>
+            </div>
+
+
+            <div class="title">
+                <h2>Piatti Disponibili:</h2>
+            </div>
+
+
+            <div class="dish">
+                <ul class="cards">
+                    <?php
+                        for($i=0;$i<50;$i++){
+                            echo'
+                            <li>
+                                <div  class="card">
+                                    <img src="images/carousel1.png" class="card__image" alt="nome cibo" />
+                                    <div class="card__overlay">
+                                        <div class="card__header">               
+                                            <div class="card__header-text">
+                                                <h3 class="card__title">Jessica Parker</h3>            
+                                            </div>
+                                        </div>
+                                        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+                                    </div>
+                                </div>      
+                            </li>';
+                            
+                        }
+                    ?>
                 </ul>
             </div>
+
+
         </div>  
     </body>
 </html>
